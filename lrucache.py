479be1a95a5818aa5f03861fb5ce5bacd63ffe56
Aleem Abdul <aleemsameer1@msitprogram.net>
2020-04-18 10:39:55 +0530

    test2

--- lrucache.py
+++ lrucache.py
@@ -7,9 +7,18 @@ class LRUCache():
     
     def put(self, key, value):
         pass
+        if self.get(key) == -1:
+            if len(self.cache) >= self.capacity:
+                self.cache.popitem(last = False)
+        self.cache[key] = value
     
     def get(self, key):
         pass
+        if key in self.cache:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+        return -1
     
     def get_cache(self):
         pass
+        return self.cache
