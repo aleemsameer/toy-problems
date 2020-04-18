72dc50b77cb7a4fc86635c2d22693d1c62a68a9c
Aleem Abdul <aleemsameer1@msitprogram.net>
2020-04-18 10:10:12 +0530

    constructor

--- lrucache.py
+++ lrucache.py
@@ -4,3 +4,12 @@ class LRUCache():
     def _init_(self, capacity):
         self.capacity = capacity
         self.cache = OrderedDict()
+    
+    def put(self, key, value):
+        pass
+    
+    def get(self, key):
+        pass
+    
+    def get_cache(self):
+        pass
